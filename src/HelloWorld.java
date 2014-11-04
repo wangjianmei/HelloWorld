68dd596171504b277e02be9b7954c1222dd3045c
wangjianmei <308629107@qq.com>
2014-11-04 18:46:44 +0800

    Test

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author cherry
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Happy!!!");
+
+	}
+
+}
